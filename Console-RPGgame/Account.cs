1ddd35b09bbbfa909bb5ccaed971a8fb33b1734d
DESKTOP-1OGKUDR\Long <nguyenhoanglong112001@gmail.com>
2024-03-11 23:24:00 +0700

    no message

---
+++ Console-RPGgame/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_RPGgame
+{
+    public class Account
+    {
+        public string username {  get;protected set; }
+        public string password { get;protected set; }
+        public Character character = null;
+
+        public Account(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+    }
+}
---
+++ Console-RPGgame/Character.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_RPGgame
+{
+    public class Character
+    {
+        public string name {  get;protected set; }
+        public CharacterType type { get; protected set; }
+        public int hp { get; protected set; }
+        public int atk {  get; protected set; }
+        public int atkspeed { get; protected set; }
+        public int ap {  get; protected set; }
+
+        public List<Item> itemuse = null;
+
+        public Character(string name, CharacterType type, int hp, int atk, int atkspeed, int ap)
+        {
+            this.name = name;
+            this.type = type;
+            this.hp = hp;
+            this.atk = atk;
+            this.atkspeed = atkspeed;
+            this.ap = ap;
+        }
+    }
+}
